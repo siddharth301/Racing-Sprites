8c8402786d006f30e86fcf5841130e68119ee39c
siddharth301 <74924743+siddharth301@users.noreply.github.com>
2021-01-27 11:34:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var sprite1,sprite2,sprite3,sprite4;
+var sprites = [];
+
+function setup(){
+createCanvas(displayWidth - 20, displayHeight-30);
+
+}
+function draw(){
+background("aqua");
+
+drawSprites();
+}
